2ea4fa650018fde2a92dc442629e518b0a5cb6a7
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-11-23 18:03:37 +0100

    images and navbar

--- nsb-site/src/App.tsx
+++ nsb-site/src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div>
-        
+      <div className="pt-24 h-screen">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
--- nsb-site/src/components/Navbar.tsx
+++ nsb-site/src/components/Navbar.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Menu } from "lucide-react";
+import logo from "../images/logo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <nav className="fixed h-24 p-4 flex justify-between w-full">
+        <img src={logo} alt="logo" className="bandLogo" />
+        <h1 className="hidden">Det Nye Norske Storband</h1>
+        <div className="hidden sm:flex gap-8 p-4 text-lg">
+          <ul>Hjem</ul>
+          <ul>Om oss</ul>
+          <ul>Musikk</ul>
+          <ul>Kontakt</ul>
+        </div>
+        <Menu className="sm:hidden w-16 h-16" />
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
