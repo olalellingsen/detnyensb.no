2523d8bc55d94604bba7aa13fb6e38776b1d84c9
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-11-23 17:24:31 +0100

    set up vite, react and tailwind

---
+++ nsb-site/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div>
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
