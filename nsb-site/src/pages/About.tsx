893987f867ff8bac7bb92bf2037561fbbe847175
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2024-01-08 15:31:21 +0100

    implement storage for images

--- nsb-site/src/pages/About.tsx
+++ nsb-site/src/pages/About.tsx
@@ -1,41 +1,50 @@
 import { useEffect, useState } from "react";
-import imageSrc from "../assets/images/about.jpg";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
-import { set } from "firebase/database";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 function About() {
   const [bio1, setBio1] = useState("");
   const [bio2, setBio2] = useState("");
   const [bio3, setBio3] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
-  // Fetch about text from database
+  const storage = getStorage();
+  const imagesRef = ref(storage, "about.jpg");
 
+  // Fetch about text and image URL from database
   useEffect(() => {
-    const checkFirestoreConnection = async () => {
+    const fetchData = async () => {
       try {
+        // Fetch text data from Firestore
         const querySnapshot = await getDocs(collection(db, "About"));
         const bioData = querySnapshot.docs.map((doc) => doc.data())[0];
 
-        console.log("About text fetched from database");
+        // Fetch image URL from Firebase Storage
+        const url = await getDownloadURL(imagesRef);
+        setImageUrl(url);
 
+        console.log("About text and image fetched from database");
         setBio1(bioData.P1);
         setBio2(bioData.P2);
         setBio3(bioData.P3);
       } catch (error) {
-        console.error("Error connecting to Firestore:", error);
+        console.error(
+          "Error connecting to Firestore or accessing Storage:",
+          error
+        );
       }
     };
 
-    // Call the function to check connection
-    checkFirestoreConnection();
-  }, []); // Empty dependency array ensures the useEffect runs only once on mount
+    // Call the function to fetch data
+    fetchData();
+  }, [imagesRef]); // Dependency on imagesRef to re-fetch data when the image changes
 
   return (
     <div className="h-full">
       <h1 className="border-b">Om oss</h1>
       <div className="pt-4 grid gap-4 mx-auto md:w-4/5 xl:w-2/3">
-        <img src={imageSrc} alt="About picture" />
+        <img src={imageUrl} alt="About picture" />
         <p>{bio1}</p>
         <p>{bio2}</p>
         <p>{bio3}</p>
