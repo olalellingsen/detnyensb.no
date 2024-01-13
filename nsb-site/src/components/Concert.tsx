2d376912f16bc5e5152d6455e3f45d6e100d30cb
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2024-01-13 13:31:08 +0100

    add picture in concert card

--- nsb-site/src/components/Concert.tsx
+++ nsb-site/src/components/Concert.tsx
@@ -1,8 +1,9 @@
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { ArrowRight, Calendar } from "lucide-react";
 import { Clock } from "lucide-react";
 import { MapPin } from "lucide-react";
 import { Ticket } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export interface ConcertProps {
   title?: string;
@@ -26,14 +27,41 @@ function Concert({
   image,
 }: ConcertProps) {
   const [showDescription, setShowDescription] = useState(false);
+  const storage = getStorage();
+  const img = ref(storage, "Concerts/" + image);
+  const [fetchedImage, setFetchedImage] = useState("");
+
+  // Fetch image URL from database
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch image URL from Firebase Storage
+        const url = await getDownloadURL(img);
+        setFetchedImage(url);
+      } catch (error) {
+        console.error(
+          "Error connecting to Firestore or accessing Storage:",
+          error
+        );
+      }
+    };
+
+    // Call the function to fetch data
+    fetchData();
+  }, [img]); // Dependency on img to re-fetch data when the image changes
+
   return (
     <div className="card grid gap-2">
       <h2 className="flex justify-start">{title}</h2>
-      <div className="grid sm:grid-cols-2 gap-4">
-        {image !== undefined && (
-          <img src={image} alt={"Image of " + title} className="rounded-md" />
+      <div className="grid sm:grid-cols-3 gap-4 py-2">
+        {image !== undefined && image !== "" && (
+          <img
+            src={fetchedImage}
+            alt={"Image of " + title}
+            className="rounded-md"
+          />
         )}
-        <div className="flex flex-wrap gap-4 sm:mt-0 md:flex-col">
+        <div className="flex flex-wrap gap-4 sm:col-span-2">
           <div className="flex gap-1">
             <Calendar />
             {date}
