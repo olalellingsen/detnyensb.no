01c463d052d46e06f2318d68f87a4960443a9d1c
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2024-01-22 18:00:09 +0100

    add standard tailwind colors

--- nsb-site/tailwind.config.js
+++ nsb-site/tailwind.config.js
@@ -2,12 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: "#bf1e2e",
-      primaryBg: "#fae8e8",
-      white: "#fff",
+    extend: {
+      colors: {
+        primary: "#bf1e2e",
+        primaryBg: "#fae8e8",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
