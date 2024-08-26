8c3f3236062b47d50f8476ffc725893b03a31f2a
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2024-08-26 20:58:12 +0200

    small bug fix in prev concerts and fetch videos from firestore

--- nsb-site/src/pages/Media.tsx
+++ nsb-site/src/pages/Media.tsx
@@ -1,22 +1,39 @@
+import { useEffect, useState } from "react";
 import Slideshow from "../components/Slideshow";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 function Media() {
+  const [youtubeVideos, setYoutubeVideos] = useState<string[]>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const query = await getDocs(collection(db, "Media"));
+        const data = query.docs
+          .sort((a, b) => a.data().order - b.data().order)
+          .map((doc) => doc.data().youtubeLink);
+        setYoutubeVideos(data);
+      } catch (error) {}
+    };
+    fetchData();
+  }, [youtubeVideos]);
+
   return (
     <section className="mainContent">
       <h1>Media</h1>
 
-      <section>
-        <iframe
-          src="https://www.youtube.com/embed/DC63I9B3K9g?autoplay=1&mute=1"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          className="w-full aspect-video py-2"
-        ></iframe>
-        <iframe
-          src="https://www.youtube.com/embed/nVf1SoZnbXQ?si=zU4QhniewRXajKhA&amp;controls=0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          className="w-full aspect-video py-2"
-        ></iframe>
-      </section>
+      {youtubeVideos && (
+        <section>
+          {youtubeVideos.map((video) => (
+            <iframe
+              src={video}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              className="w-full aspect-video py-2"
+            ></iframe>
+          ))}
+        </section>
+      )}
       <br />
       <section>
         <Slideshow />
